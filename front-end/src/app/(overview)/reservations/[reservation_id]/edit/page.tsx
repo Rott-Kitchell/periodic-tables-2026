3f1917ce3f6af6ca8169a9d6512f3722ea1a2d2d
Rott Kitchell <r.kitchell815@gmail.com>
2026-09-25 05:13:11 -0500

    fix: update heading margin for consistency across reservation pages

--- front-end/src/app/(overview)/reservations/[reservation_id]/edit/page.tsx
+++ front-end/src/app/(overview)/reservations/[reservation_id]/edit/page.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
 export default async function Page() {
   return (
     <main>
-      <h1 className="text-center font-bold text-3xl mb-6">Edit Reservation</h1>
+      <h1 className="text-center font-bold text-3xl my-6">Edit Reservation</h1>
     </main>
   );
 }
